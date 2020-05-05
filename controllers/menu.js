09c79287bede8c3aec44776f28c6aba51982b2fb
Claudio Stuardo <claudio.stuardo96@gmail.com>
2020-05-05 00:44:18 -0400

    menu bugfix

--- controllers/menu.js
+++ controllers/menu.js
@@ -36,7 +36,47 @@ function getMenus(req, res) {
     });
 }
 
+function updateMenu(req, res) {
+    let menuData = req.body;
+    const params = req.params;
+
+    Menu.findByIdAndUpdate(params.id, menuData, (err, menuUpdate) => {
+        if (err) {
+            res.status(500).send({message: "Error del servidor"})
+        } else {
+            if (!menuUpdate) {
+                res.status(404).send({message: "No se ha encontrado ningún menu"});
+            } else {
+                res.status(200).send({message: "Menú actualizado"});
+            }
+        }
+    });
+}
+
+function activateMenu(req, res) {
+    const { id } = req.params;
+    const { active } = req.body;
+
+    Menu.findByIdAndUpdate(id, { active }, (err, menuStored) => {
+        if (err) {
+            res.status(500).send({message: "Error del servidor"});
+        } else {
+            if (!menuStored) {
+                res.status(404).send({message: "Menu no encontrado"});
+            } else {
+                if (active) {
+                    res.status(200).send({message: "Menu activado"});
+                } else {
+                    res.status(200).send({message: "Menu desactivado"});
+                }
+            }
+        }
+    });
+}
+
 module.exports = {
     addMenu,
-    getMenus
+    getMenus,
+    updateMenu,
+    activateMenu
 }
